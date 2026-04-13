81bccd6e6a769b0f78f3f8b613d20fa7a28541e6
Shafwan <arul.safwan73@gmail.com>
2026-04-13 16:38:58 +0800

    feat: add ContactView and ProjectView components

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
-    <h1>Project</h1>
+  <section class="min-h-screen flex flex-col items-center justify-center py-16 px-8 gap-12">
+    <div class="text-center">
+      <h1 class="text-[clamp(2.5rem,6vw,5rem)] font-extrabold tracking-tighter m-0">Projects</h1>
+      <p class="text-lg opacity-50 mt-2">Creative work &amp; experiments</p>
+    </div>
+    <div class="flex justify-center items-center">
+        
+    </div>
+  </section>
 </template>
